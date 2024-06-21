a35845d9b0399c778ef325dffc1605b3d0c65754
Aliny Melquiades <alinymelquiadesdesouza@gmail.com>
2024-06-21 08:50:37 -0300

    Update Registro_De_Tabelas.sql

--- Registro_De_Tabelas.sql
+++ Registro_De_Tabelas.sql
@@ -10,9 +10,9 @@ create table filmes
 	fk_cod_gen int not null
 )
 
-create table cliente
+create table clientes
 (
-    cod_cli int primary key,
+        cod_cli int primary key,
 	cpf char(11) not null,
 	nome varchar(60) not null,
 	telefone varchar(10) not null,
@@ -22,16 +22,16 @@ create table cliente
 
 create table locacao_filme
 (
-    fk_cod_loc int not null,
-    fk_cod_filme int not null,
-    valor numeric(15,2) not null,
+        fk_cod_loc int not null,
+        fk_cod_filme int not null,
+        valor numeric(15,2) not null,
 	num_dias int not null,
 	data_devol date not null
 )
 
 create table locacao
 (
-    cod_loc int primary key,
+        cod_loc int primary key,
 	data_loc date not null,
 	desconto numeric(15,2) not null,
 	multa numeric(15,2) not null,
@@ -42,7 +42,7 @@ create table locacao
 
 create table filme_ator
 (
-    fk_cod_ator int not null,
+        fk_cod_ator int not null,
 	fk_cod_filme int not null,
 	ator char(1) not null,
 	diretor char(1) not null
@@ -50,7 +50,7 @@ create table filme_ator
 
 create table ator
 (
-    cod_ator int primary key,
+        cod_ator int primary key,
 	nome varchar(60) not null
 )
 
@@ -62,21 +62,21 @@ create table genero
 
 create table categoria
 (
-    cod_cat int not null,
+        cod_cat int not null,
 	nome varchar(60) not null,
 	valor numeric(15,2) not null
 )
 
 create table dependente
 (
-    fk_cod_cli int not null,
+       fk_cod_cli int not null,
 	fk_cod_dep int not null,
 	parentesco varchar(20) not null
 )
 
 create table endereco
 (
-    cod_end int primary key,
+        cod_end int primary key,
 	logradouro varchar(40) not null,
 	tipo_log varchar(40) not null,
 	complemento varchar(20) not null,
@@ -89,15 +89,15 @@ create table endereco
 
 create table cli_endereco
 (
-    fk_cod_end int not null,
-    fk_cod_cli int not null
+       fk_cod_end int not null,
+       fk_cod_cli int not null
 	
 )
 
 create table profissao
 (
-	 cod_prof int primary key not null,
-	 nome varchar(60) not null
+	cod_prof int primary key not null,
+	nome varchar(60) not null
 )
 
 
